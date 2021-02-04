03a85321d05689ddbae455fb5bbe5a4da40ad99f
anhnguyenids <anh.nguyen@idsolutions.com.vn>
2021-02-04 18:16:29 +0700

    add clear css + update code

--- src/index.php
+++ src/index.php
@@ -6,6 +6,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/style.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
@@ -15,38 +16,147 @@
 
 <body>
 
-    <div class="container-fluid">
+    <div class="container-fluid p-0">
         <header>
-            <div class="row">
-                <div class="col-3">
-                    <div>
-                        <i class="fas fa-street-view"></i>
-                    </div>
-                    <div>
-                        <label>Showroom Directions</label>
-                        <span>2600 24th St N St Petersburg FL 33713</span>
-                    </div>
-                </div>
-                <div class="col-3">
-                    <div>
-                        <i class="fas fa-street-view"></i>
-                    </div>
-                    <div>
-                        <label>Showroom Directions</label>
-                        <span>2600 24th St N St Petersburg FL 33713</span>
-                    </div>
-                </div>
-                <div class="col-3">
-                    <div>
-                        <i class="fas fa-street-view"></i>
-                    </div>
-                    <div>
-                        <label>Showroom Directions</label>
-                        <span>2600 24th St N St Petersburg FL 33713</span>
+            <div class="header py-2">
+                <div class="container">
+                    <div class="d-flex justify-content-between">
+                        <div>
+                            <div class="show-room">
+                                <i class="fa fa-map-marker"></i>
+                                <p>
+                                    <span>Showroom Directions</span>
+                                    2600 24th St N St Petersburg FL 33713
+                                </p>
+                            </div>
+                        </div>
+                        <div>
+                            <div class="contact-us">
+                                <i class="fa fa-envelope"></i>
+                                <p>
+                                    <span>Email Us</span>
+                                    tampaBayPavers@gmail.com
+                                </p>
+                            </div>
+                        </div>
+                        <div class="d-flex align-item-end">
+                            <div class="call-us">
+                                <i class="fa fa-phone"></i>
+                                <p>
+                                    <span>Call Us</span>
+                                    727 - 342 - 3241
+                                </p>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
+
+            <div class="lower-header text-center py-4 text-white" style="background-color: #0064af; width:100%">
+                <h1 style="font-size: 38px;">TAMPA BAY PAVER & HARDSCAPES</h1>
+                <h3 class="py-2" style="font-size: 25px;">The Original and Still The Best Tampa Bay Pavers</h3>
+                <p>Florida State Certified Builders License - CBC1261248</p>
+            </div>
         </header>
+        <!-- content -->
+        <div class="main row no-gutters">
+            <!-- content left -->
+            <div class="col-3" style="background-color: #000000;min-height: 1000px;">
+                <!-- navi -->
+                <div class="nav-side-menu">
+                    <div class="brand">MENU SITE</div>
+                    <i class="fa fa-bars fa-2x toggle-btn" data-toggle="collapse" data-target="#menu-content"></i>
+
+                    <div class="menu-list">
+
+                        <ul id="menu-content" class="menu-content collapse out">
+                            <li class="active">
+                                <a href="#">Home</a>
+                            </li>
+                            <li>
+                                <a href="#">UI Elements</a>
+                            </li>
+                            <li>
+                                <a href="#">Services</a>
+                            </li>
+                            <li>
+                                <a href="#">New</a>
+                            </li>
+                            <li>
+                                <a href="#">Profile</a>
+                            </li>
+                            <li>
+                                <a style="border:none;" href="#">Users</a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+            <div class="main-content col-9" style="background-color: #202020;">
+                <!-- slider -->
+                <div style="padding: 70.5px 70.5px 0 70.5px;">
+                    <div style="padding:10px; background-color:#ffffff; width:inherit; height:calc((75vw - 161px)/ 1.55);">
+                        <div id="carouselExampleIndicators" style="height: 100%;border: 1px solid;" class="carousel slide" data-ride="carousel">
+                            <ol class="carousel-indicators">
+                                <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+                                <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+                                <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+                            </ol>
+                            <div class="carousel-inner">
+                                <div class="carousel-item active">
+                                    <img class="d-block w-100" src="..." alt="First slide">
+                                </div>
+                                <div class="carousel-item">
+                                    <img class="d-block w-100" src="..." alt="Second slide">
+                                </div>
+                                <div class="carousel-item">
+                                    <img class="d-block w-100" src="..." alt="Third slide">
+                                </div>
+                            </div>
+                            <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+                                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                                <span class="sr-only">Previous</span>
+                            </a>
+                            <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+                                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                                <span class="sr-only">Next</span>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+                <!-- who we are -->
+                <div class="mt-4">
+                    <article class="who-we-are text-white">
+                        <h1 class="py-4 text-center who-we-are-title">
+                            <div class="title-tiret"></div>
+                            <span>WHO WE ARE</span>
+                        </h1>
+                        <div>
+                            <P>- People have been dreaming in stone for more than 2,000 year. ancient Tomans built roads
+                                that are still being used today!</P>
+                            <p>- As more and more people discover the enjoyment of outdoor living, one of the top
+                                priotities is often the construcion of a driveway, walkway, patio, pool area, outdoor
+                                kitchen or a place where they can relax. This area becomes an attractive outdoor room
+                                for work or play. It is a place to read the Sunday paper, eat a meal, play with the
+                                children, enjoy the sun, or entertain friends.</p>
+                            <p>- This outdoor living space is an important asset, increasing the value of a home as well
+                                as reclaim unused areas of lawn and garden. The selection of the color and texture of
+                                the pavers can be chosen to blend with the tone and scale of the hourse. A patio,
+                                walkway or garden wall can connect the house with various parts of the landscape and
+                                define spaces within the yard.</p>
+                            <p>- Whether you entertain social gatherings, Sunday Night Football or relaxing family
+                                dinner by the pool, we can build an outdoor kitchen that accomodates all your needs.
+                                With our design and construction skills, we can work with any space, large or small,
+                                to build the outdoor experience that you and your family will enjoy for years to come.
+                                Our designed kitchens are made to fit into your outside hardscape. A patio kitchen
+                                can be built with many of the same amenities of an indoor kitchen along with wood
+                                grills and smokers.</p>
+                        </div>
+
+                    </article>
+                </div>
+            </div>
+        </div>
     </div>
 
 </body>
